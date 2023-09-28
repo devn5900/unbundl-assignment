d4771bcf07a2c23ddd1403b8cb5772bf308fc93f
devn5900 <devn5900@gmail.com>
2023-09-28 20:28:53 +0530

    task 1:- HomePage :DONE

--- task-1/src/redux/actionTypes.js
+++
@@ -1,5 +0,0 @@
-
-export const ADD_TO_CART= 'add/to/cart';
-export const REMOVE_FROM_CART='remove/from/cart';
-export const INCREASE_QUANTITY='increase/quantity';
-export const DECREASE_QUANTITY='decrease/quantity';
